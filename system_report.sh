3a260cd84c97a74211979b650e4be3e6fdcfcc95
Jaybhuke <Jaybhuke97@gmail.com>
2026-04-29 19:10:06 +0000

    Push final changes

--- system_report.sh
+++ system_report.sh
@@ -3,21 +3,21 @@
 REPORT="/home/ec2-user/learning/projects/sysht/report.txt"
 DATE=$(date)
 
-
+echo "--------------------" >> $REPORT
 echo "System Health Report $DATE" >> $REPORT
 echo "--------------------" >> $REPORT
 
-echo "CPU Usage" >> $REPORT
+echo "-----------CPU Usage-------------" >> $REPORT
 top -bn1 | grep "Cpu(s)" >> $REPORT
 echo ""
 
-echo "Memory Usage" >> $REPORT
+echo "-----------Memory Usage-----------" >> $REPORT
 free -m >> $REPORT
 echo ""
 
-echo "Disk usage" >> $REPORT
+echo "-----------Disk usage-------------" >> $REPORT
 df -h >> $REPORT
 echo ""
 
-echo "Top Processes" >> $REPORT
+echo "-----------Top Processes-----------" >> $REPORT
 ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | head -n 6 >> $REPORT
